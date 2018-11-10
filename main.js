75e55a0553f18bbb0c89e4af591f007341e06ea4
huiyihuiyixiu <624286257@qq.com>
2018-11-10 15:59:08 +0800

    添加清除，保存功能

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var yyy = document.getElementById('xxx');
 var context = yyy.getContext('2d');
-
+var lineWidth = 5;
 autoSetCanvasSize(yyy)
 
 listenToUser(yyy)
@@ -16,6 +16,19 @@ rubber.onclick = function(){
   eraserEnabled = true
   rubber.classList.add('active')
   pen.classList.remove('active')
+}
+clear.onclick = function(){
+  context.clearRect(0, 0, yyy.width, yyy.height);
+}
+download.onclick = function(){
+  var url = yyy.toDataURL("image/png")
+  console.log(url)
+  var a =document.createElement('a')
+  document.body.appendChild(a)
+  a.href = url
+  a.download = '我的画儿'
+  a.target = '_blank'
+  a.click()
 }
 
 red.onclick = function(){
@@ -63,6 +76,12 @@ black.onclick = function(){
   yellow.classList.remove('active')
   black.classList.add('active')
 }
+thin.onclick = function(){
+  lineWidth = 5
+}
+thick.onclick = function(){
+  lineWidth = 10
+}
 
 /******/
 
@@ -91,7 +110,7 @@ function drawCircle(x, y, radius) {
 function drawLine(x1, y1, x2, y2) {
   context.beginPath();
   context.moveTo(x1, y1) // 起点
-  context.lineWidth = 5
+  context.lineWidth = lineWidth
   context.lineTo(x2, y2) // 终点
   context.stroke()
   context.closePath()
